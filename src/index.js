3911c25adbc9a3d92e59b8dbc81b84b584b892a9
cthacker-udel <cthacker@udel.edu>
2021-09-15 04:31:00 -0400

    added computer guesses

--- src/index.js
+++ src/index.js
@@ -2,22 +2,26 @@ import React, {useState} from 'react';
 import ReactDOM, { render } from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Image from 'react-bootstrap/Image';
 import Table from 'react-bootstrap/Table';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import Badge from 'react-bootstrap/Badge'
 
 function Board(props){
 
+
+    const printOut = (event) => {
+
+        console.log(event.target.name); // acquire button name that is pressed
+        
+    }
 
     const generateSpace = (theName) => {
 
         return(
 
-            <Button name={theName} variant="outline-primary">{theName}</Button>
+            <Button name={theName} variant="outline-primary" onClick={printOut} id={theName}>{theName}</Button>
 
         );
 
@@ -26,6 +30,7 @@ function Board(props){
     console.log(`props = ${props['theOptions']}`);
 
     const [spaces,setSpaces] = useState(props['theOptions']);
+    const [computerChoices,setComputerChoices] = useState('');
 
     console.log(`spaces = ${spaces}`);
 
@@ -182,9 +187,29 @@ function NavigationButton(){
 
 function MainPage(){
 
+    const theChoices = [['A1','B1','C1','D1','E1'],['A2','B2','C2','D2','E2'],['A3','B3','C3','D3','E3'],['A4','B4','C4','D4','E4'],['A5','B5','C5','D5','E5']];
 
-    const [options,setOptions] = useState([['A1','B1','C1','D1','E1'],['A2','B2','C2','D2','E2'],['A3','B3','C3','D3','E3'],['A4','B4','C4','D4','E4'],['A5','B5','C5','D5','E5']]);
+    const [options,setOptions] = useState(theChoices);
     const [turn,setTurn] = useState(true); // user goes first if true
+
+    // get computer guesses
+
+    // default to 5 guesses <--- may add difficulty functionality later on (reset board, regenerate guesses) also perhaps add customizable functionality such as 
+    // the user determining the board size rather than default 5x5, also maybe add implementation so instead of picking 5 random points separately, they are conjoined
+    // together to form a "ship"
+
+    let compGuess = [options[0][Math.random() * (4)]
+                        ,options[1][Math.random() * 4]
+                        ,options[2][Math.random() * 4]
+                        ,options[3][Math.random() * 4]
+                        ,options[4][Math.random() * 4]];
+
+    const[computerGuess,setComputerGuess] = useState(compGuess);
+
+    const [misses,setMisses] = useState(0);
+    const [hits,setHits] = useState(0);
+    const [sank,setSank] = useState(0);
+
 
     return(
         <>
@@ -192,11 +217,30 @@ function MainPage(){
 
                 <Col>
 
-                <Board theOptions={options}/>
+                <Board theOptions={options} theCompGuesses={computerGuess}/>
 
                 </Col>
 
             </Row>
+            <Row style={{textAlign: "center"}}>
+                <Col>
+
+                    <Badge bg="primary" id="missesBadge">Misses : 0</Badge>
+
+                </Col>
+                <Col>
+
+                    <Badge bg="primary" id="hitsbage">Hits : 0</Badge>
+
+                </Col>
+                <Col>
+
+                    <Badge bg="primary" id="shipsank">Sank : 0</Badge>
+
+                </Col>
+            </Row>
+            <br />
+            <br />
             <Row>
                 <Col>
 
